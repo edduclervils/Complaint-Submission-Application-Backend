00999c21cc0d7bd53c3c9162641d420632849c24
edduclervils <edduclervils@gmail.com>
2023-03-13 17:08:59 -0400

    Initial commit. Simple Skeleton

---
+++ src/main/java/com/revature/ComplaintSubmissionApplication/ComplaintSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ComplaintSubmissionApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintSubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/ComplaintSubmissionApplication/ComplaintSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ComplaintSubmissionApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
