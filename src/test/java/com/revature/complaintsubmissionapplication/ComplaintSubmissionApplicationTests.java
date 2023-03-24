12da000b83fd513e4b8635b18224d988c2c98d55
edduclervils <edduclervils@gmail.com>
2023-03-24 01:02:43 -0400

    Updates for testing

--- src/test/java/com/revature/complaintsubmissionapplication/ComplaintSubmissionApplicationTests.java
+++ src/test/java/com/revature/complaintsubmissionapplication/ComplaintSubmissionApplicationTests.java
@@ -2,12 +2,15 @@ package com.revature.complaintsubmissionapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:test.properties")
 class ComplaintSubmissionApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println(System.getenv("RDS_URL"));
 	}
 
 }
