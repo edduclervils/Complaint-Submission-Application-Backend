9afb5bcbb65f73e74d2c284cb5603bfdb2ddffea
edduclervils <edduclervils@gmail.com>
2023-03-24 03:25:13 -0400

    Gotta get this working

--- src/main/java/com/revature/complaintsubmissionapplication/ComplaintSubmissionApplication.java
+++ src/main/java/com/revature/complaintsubmissionapplication/ComplaintSubmissionApplication.java
@@ -2,8 +2,10 @@ package com.revature.complaintsubmissionapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:prod.properties")
 public class ComplaintSubmissionApplication {
 
 	public static void main(String[] args) {
